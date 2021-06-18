5497e88c237978d43a604f714639da4201556d76
brewchetta <chett.tiller@gmail.com>
2021-06-18 10:57:23 -0400

    steps one and two

--- app/index.js
+++ app/index.js
@@ -1,5 +1,26 @@
+// Global constants
+const URL = 'http://localhost:3000/furniture'
+const furnitureList = document.querySelector('ul')
+let furnitureArray
+
 // step one: fetch all furniture
 
-// step two: add all furniture to the ul
+fetch(URL)
+.then(response => response.json())
+.then(fetchedFurniture => {
+  furnitureArray = fetchedFurniture
+  // step two: add all furniture to the ul
+
+  furnitureArray.forEach(furniture => {
+    const li = document.createElement('li')
+    li.textContent = furniture.name
+    li.dataset.id = furniture.id
+    furnitureList.append(li)
+  })
+
+})
+
+
+
 
 // step three: ???
